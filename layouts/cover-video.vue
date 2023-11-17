f37c11b590647d50709f59d8a6f449a89a104154
宇井 陸登 <ui@appleple.com>
2023-11-17 17:07:42 +0900

    色々修正

---
+++ layouts/cover-video.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  video: {
+    default: '',
+  },
+})
+
+</script>
+
+<template>
+  <div class="">
+    <video :src="video" class="h-full w-full m-auto" loop controls autoplay muted ></video>
+  </div>
+</template>
